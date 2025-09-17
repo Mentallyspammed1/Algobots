d2e645950eff1a27b1a1f01fde5b49a7a5c4fce5
Worldguide <jeremiahdryden@yahoo.com>
2025-09-17 17:05:52 -0500

    ok

---
+++ pybit/javabot/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\\.js$': '<rootDir>/node_modules/babel-jest'
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.js',
+    '**/tests/**/*.test.js'
+  ],
+  testRunner: 'jest-circus',
+};
